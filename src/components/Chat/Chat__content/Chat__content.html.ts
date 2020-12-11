89f5c111185f4d7f4375dd7efff284ed2f916a91
dvs <dvs@crcr.ru>
2020-12-11 16:58:50 +0500

    edit button styles

--- src/components/Chat/Chat__content/Chat__content.html.ts
+++ src/components/Chat/Chat__content/Chat__content.html.ts
@@ -6,7 +6,7 @@ export default `
   </div>
   <div class="chat__header-actions">
     <div class="dropdown bottom_right">
-      <button class="dropdown__toggle btn-round btn-round_transparent chat__header-button">
+      <button class="dropdown__toggle button-round button-round_transparent chat__header-button">
         <svg width="3" height="15" viewBox="0 0 3 15" fill="#757575" xmlns="http://www.w3.org/2000/svg">
           <path d="M3 1.5C3 2.32843 2.32843 3 1.5 3C0.671573 3 0 2.32843 0 1.5C0 0.671573 0.671573 0 1.5 0C2.32843 0 3 0.671573 3 1.5Z" />
           <path d="M3 13.5C3 14.3284 2.32843 15 1.5 15C0.671573 15 0 14.3284 0 13.5C0 12.6716 0.671573 12 1.5 12C2.32843 12 3 12.6716 3 13.5Z" />
@@ -32,7 +32,7 @@ export default `
     </div>
   </div>
   <input class="chat__message-text" name="message" type="text" placeholder="Сообщение">
-  <button type="button" class="btn-round btn-round_primary chat__message-send">
+  <button type="button" class="button-round button-round_primary chat__message-send">
     <i class="fa fa-arrow-right"></i>
   </button>
 </form>
