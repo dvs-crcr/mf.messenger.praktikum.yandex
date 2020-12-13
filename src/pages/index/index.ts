69ac168e8779e9c365ddedce7699a16eae73d781
dvs <dvs@crcr.ru>
2020-12-13 20:31:47 +0500

    remove blank targets

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -4,7 +4,7 @@ import { Custom } from './../../blocks/Custom/Custom.js'
 
 const linksTemplate = `
 <a target="{{target}}" href="{{href}}">{{title}}</a>
-<span>{{descr}}</span>
+{{descr}}
 `;
 
 const links = [
@@ -12,11 +12,11 @@ const links = [
   { target:'_blank', href: '/registration.html', title: 'Регистрация', descr: ' - макет страницы регистрации' },
   { target:'_blank', href: '/chat-list.html', title: 'Список чатов', descr: ' - макет страницы со списком чатов' },
   { target:'_blank', href: '/chat.html', title: 'Чат', descr: ' - макет страницы c лентой переписки' },
-  { target:'', href: '/profile.html', title: 'Профиль', descr: ' - макет страницы c профилем пользователя' },
-  { target:'', href: '/profile_edit.html', title: 'Редактирование профиля', descr: ' - макет страницы с настройкой пользователя' },
-  { target:'', href: '/profile_password.html', title: 'Пароль профиля', descr: ' - макет страницы для смены пароля профиля' },
-  { target:'', href: '/404.html', title: 'Ошибка 404', descr: ' - макет страницы c ошибкой 404' },
-  { target:'', href: '/500.html', title: 'Ошибка 500', descr: ' - макет страницы c ошибкой 5**' }
+  { href: '/profile.html', title: 'Профиль', descr: ' - макет страницы c профилем пользователя' },
+  { href: '/profile_edit.html', title: 'Редактирование профиля', descr: ' - макет страницы с настройкой пользователя' },
+  { href: '/profile_password.html', title: 'Пароль профиля', descr: ' - макет страницы для смены пароля профиля' },
+  { href: '/404.html', title: 'Ошибка 404', descr: ' - макет страницы c ошибкой 404' },
+  { href: '/500.html', title: 'Ошибка 500', descr: ' - макет страницы c ошибкой 5**' }
 ];
 
 const indexList = new Custom('nav', {
