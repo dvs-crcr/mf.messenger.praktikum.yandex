67d8a9d4d4a25a1d373d29108fa3d4c5f177379c
dvs <dvs@crcr.ru>
2021-02-18 01:28:53 +0500

    refactor app init

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 
-const PORT = 3000;
+class App {
+  constructor(options) {
+    this._options = options;
+    this._app = express();
+  }
 
-/** DevLog */
-_log = (...msg) => {
-  if (process.argv.includes('--dev')) {
-    console.log(...msg);
+  init = async () => {
+    const { port = 3000, routes = {} } = this._options;
+    await this._init_routes(routes);
+    await this._listen(port)
+  }
+
+  _init_routes = async (routes) => {
+    routes.forEach((route) => {
+      this._app.use(...route);
+    })
+  }
+
+  _listen = async (port) => {
+    this._app.listen(port, () => {
+      this._log(`APP running on port: ${port}`);
+    });
+  }
+
+  _log = (...msg) => {
+    if (process.argv.includes('--dev')) {
+      console.log(...msg);
+    }
   }
 }
 
-app.use('/src', express.static(path.join(__dirname, 'src')));
-app.use(express.static(path.join(__dirname, 'static')));
+(async () => {
+  const appServer = new App({
+    routes: [
+      ['/src', express.static(path.join(__dirname, 'src'))],
+      ['/', express.static(path.join(__dirname, 'static'))]
+    ],
+  });
+  await appServer.init();
+})();
 
-app.listen(PORT, () => {
-  _log(`APP running on port: ${PORT}`);
-});
+
